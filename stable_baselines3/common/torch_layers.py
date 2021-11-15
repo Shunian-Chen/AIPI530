9dbd973cd88450a4e2f7a830c12a3e9a19f8061a
chuanmingwang <40827070+chuanmingwang@users.noreply.github.com>
2021-11-15 00:39:39 -0500

    CNN structure modified

--- stable_baselines3/common/torch_layers.py
+++ stable_baselines3/common/torch_layers.py
@@ -76,9 +76,10 @@ class NatureCNN(BaseFeaturesExtractor):
         self.cnn = nn.Sequential(
             nn.Conv2d(n_input_channels, 32, kernel_size=8, stride=4, padding=0),
             nn.ReLU(),
+            nn.MaxPool2d(2, 2),
             nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=0),
             nn.ReLU(),
-            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=0),
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=0),
             nn.ReLU(),
             nn.Flatten(),
         )
